346693d923007de83a399ac26978dc23521b63bf
Abner Vital <avgchaves@gmail.com>
2025-04-22 17:19:51 -0300

    refactor: aplicar herança com superclasse Titulo e reorganizar classes Filme e Serie

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,44 +1,5 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacao;
-    private int duracaoEmMinutos;
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public int getTotalDeAvaliacao(){
-        return totalDeAvaliacao;
-    }
-
-    public void exibeFichaTecnica () {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacao++;
-    }
-
-    public double pegaMedia () {
-        return somaDasAvaliacoes/totalDeAvaliacao;
-    }
+public class Filme extends Titulo {
+    private String diretor;
 }
